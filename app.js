1bdc316546099a223fda97d069eee759b2904878
JorgeUcsin <2019030422@upsin.edu.mx>
2022-03-07 16:03:08 -0700

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+// codificar request 
+
+app.get('/', (req, res) => {
+    res.send("Iniciamos Servidor")
+})
+
+const puerto = 3000;
+
+app.listen(puerto, () => {
+    console.log("Escuchando");
+})
